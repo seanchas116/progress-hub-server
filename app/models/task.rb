cdae3b93883afbf576166379584149c9bcc8cab0
Ryohei Ikegami <iofg2100@gmail.com>
2015-01-12 20:03:56 +0900

    Fix TaskProgressStatus

--- app/models/task.rb
+++ app/models/task.rb
@@ -28,17 +28,22 @@ class Task < ActiveRecord::Base
 
   validates :url, allow_blank: true, format: URI.regexp(%w(http https))
 
-  after_create do
+  after_create :record_addition
+  before_save :record_progress
+
+  def record_addition
     TaskAdditionStatus.create!(user: project.user, task: self)
   end
 
-  before_save do
-    if stage_changed? && stage_was.present?
-      TaskProgressStatus.create!(
-        user: project.user,
-        task: self,
-        task_stage_before: stage_was,
-        task_stage_after: stage)
-    end
+  def record_progress
+    return unless stage_changed?
+    return unless stage_was.present?
+    return unless STAGES.index(stage_was.to_sym) < STAGES.index(stage.to_sym)
+
+    TaskProgressStatus.create!(
+      user: project.user,
+      task: self,
+      task_stage_before: stage_was,
+      task_stage_after: stage)
   end
 end
--- spec/models/task_progress_status_spec.rb
+++ spec/models/task_progress_status_spec.rb
@@ -3,17 +3,29 @@ require 'rails_helper'
 describe TaskProgressStatus, type: :model do
   let(:user) { FactoryGirl.create(:user) }
   let(:project) { FactoryGirl.create(:project, user: user) }
-  let!(:task) do
-    FactoryGirl.create(:task, project: project).tap do |task|
+  let(:task) { FactoryGirl.create(:task, project: project) }
+  let(:status) { TaskProgressStatus.where(user: user).first }
+
+  context 'move stageu from todo to done' do
+    let(:original_status) { :todo }
+    before do
       task.stage = :done
       task.save!
     end
-  end
-  let(:status) { user.statuses.last }
 
-  it { expect(status).to be_a(TaskProgressStatus) }
-  it { expect(status.user).to eq(user) }
-  it { expect(status.task).to eq(task) }
-  it { expect(status.task_stage_before.todo?).to eq(true) }
-  it { expect(status.task_stage_after.done?).to eq(true) }
+    it { expect(status).to be_a(TaskProgressStatus) }
+    it { expect(status.user).to eq(user) }
+    it { expect(status.task).to eq(task) }
+    it { expect(status.task_stage_before.todo?).to eq(true) }
+    it { expect(status.task_stage_after.done?).to eq(true) }
+  end
+
+  context 'move stage from done to todo' do
+    let(:original_status) { :done }
+    before do
+      task.stage = :todo
+      task.save!
+    end
+    it { expect(status).to be_nil }
+  end
 end
